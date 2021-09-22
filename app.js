68b2e8dc6d06a2c5fabdcd1d4267b6e06c1e87bc
YainSotelo <yainsotelo3@gmail.com>
2021-09-22 10:18:08 -0300

    Endpoint alumno

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use('/alumno',require('./endpoint/alumno.endpoint'))
+
+
+app.listen(port,()=>{
+    console.log(`app running on ${port}`)
+})
+
+
---
+++ endpoint/alumno.endpoint.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.send('alumno')
+})
+
+
+
+module.exports = router
